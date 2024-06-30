aad3224b049e5794a27039abe1d092177274de03
Omar Ahmed <omarahmed19991@outlook.com>
2024-06-30 15:14:04 +0300

    Adding JavaScript Dates problem

---
+++ JavaScript_Dates.js
@@ -0,0 +1,31 @@
+function getDayName(dateString) {
+    let dayName;
+    // Write your code here
+    dayName = new Date(dateString).getDay();
+    switch (dayName) {
+        case 0:
+            dayName = "Sunday";
+            break;
+        case 1:
+            dayName = "Monday";
+            break;
+        case 2:
+            dayName = "Tuesday";
+            break;
+        case 3:
+            dayName = "Wednesday";
+            break;
+        case 4:
+            dayName = "Thursday";
+            break;
+        case 5:
+            dayName = "Friday";
+            break;
+        case 6:
+            dayName = "Saturday";
+            break;
+        default:
+            break;
+    }
+    return dayName;
+}
